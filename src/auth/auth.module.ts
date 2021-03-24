d417b8de31cbe332f7b4df4780088e3fce42642d
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2021-03-24 22:49:51 +0100

    sync

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,16 @@
-import {Module} from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import {ConfigModule, ConfigService} from '@nestjs/config';
 import {JwtModule} from '@nestjs/jwt';
 import {AuthService} from './auth.service';
 import {AuthController} from './auth.controller';
+import { RolesGuard } from './guards/roles-guard';
+import { JwtAuthGuard } from './guards/jwt-guard';
+import { JwtStrategy } from './guards/jwt-strategy';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
     imports: [
+        forwardRef(() => UserModule),
         JwtModule.registerAsync({
             imports: [ ConfigModule ],
             inject: [ ConfigService ],
@@ -15,7 +20,7 @@ import {AuthController} from './auth.controller';
             })
         })
     ],
-    providers: [ AuthService ],
+    providers: [ AuthService, RolesGuard, JwtAuthGuard, JwtStrategy ],
     controllers: [ AuthController ],
     exports: [ AuthService ]
 })
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,7 @@
-import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post, Put, UseGuards} from '@nestjs/common';
+import { hasRoles } from 'src/auth/decorators/roles.decorator';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-guard';
+import { RolesGuard } from 'src/auth/guards/roles-guard';
 import {User} from './user.interface';
 import {UserService} from './user.service';
 
@@ -12,6 +15,8 @@ export class UserController {
         return this.userService.create(user).pipe()
     }
 
+    @hasRoles('Admin')
+    @UseGuards(JwtAuthGuard, RolesGuard)
     @Get('')
     findAllUsers() {
         return this.userService.findAll()
