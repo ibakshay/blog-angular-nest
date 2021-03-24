d0d7e9650a45314f23a0818f175df56186248523
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2021-03-24 23:00:28 +0100

    sync

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,31 @@
 import {Body, Controller, Delete, Get, Param, Post, Put, UseGuards} from '@nestjs/common';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { hasRoles } from 'src/auth/decorators/roles.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-guard';
 import { RolesGuard } from 'src/auth/guards/roles-guard';
 import {User} from './user.interface';
 import {UserService} from './user.service';
 
-@Controller('user')
+@Controller('users ')
 export class UserController {
 
     constructor(private userService: UserService) { }
-
     @Post()
-    createUser(@Body() user: User) {
-        return this.userService.create(user).pipe()
+    create(@Body() user: User): Observable<User | Object> {
+        return this.userService.create(user).pipe(
+            map((user: User) => user),
+            catchError(err => of({ error: err.message }))
+        );
+    }
+
+    @Post('login')
+    login(@Body() user: User): Observable<Object> {
+        return this.userService.login(user).pipe(
+            map((jwt: string) => {
+                return { access_token: jwt };
+            })
+        )
     }
 
     @hasRoles('Admin')
